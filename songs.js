a2fd4aadea6a51dcf2f6fce8d02f1eb8b92bf158
egpennington <egpennington@hotmail.com>
2024-01-13 12:27:54 -0600

    added 3 new songs

--- songs.js
+++ songs.js
@@ -113,7 +113,258 @@ const availableSongs = [
         `,
         audio: "https://open.spotify.com/embed/track/5gAYXQdlgjsl6HIle07sDq?si=3196380d1a654fbc",
         spotifyEmbedURL: "https://open.spotify.com/embed/track/3vwqigHwHd0GMaAtUo7PNH?si=9535c92c59274910"
-    },    
+    },
+    {
+        name: "Come on Eileen",
+        lyrics: `
+            Come on, Eileen
+            Come on, Eileen
+            Poor old Johnnie Ray
+            Sounded sad upon the radio
+            Moved a million hearts in mono
+            Our mothers cried
+            Sang along, who'd blame them?
+            You've grown (you're grown up)
+            So grown (so grown up)
+            Now I must say more than ever
+            (Come on, Eileen)
+            Too-ra-loo-ra
+            Too-ra-loo-rye-ay
+            And we can sing just like our fathers
+            Come on, Eileen
+            Oh, I swear (what he means)
+            At this moment
+            You mean everything
+            You in that dress
+            My thoughts, I confess
+            Verge on dirty
+            Ah, come on, Eileen
+            Come on, Eileen
+            These people 'round here
+            Were beaten down, eyes sunk in smoke-dried face
+            They're so resigned to what their fate is
+            But not us (no never)
+            But not us (not ever)
+            We are far too young and clever
+            (Remember)
+            Too-ra-loo-ra
+            Too-ra-loo-rye-ay
+            And you'll hum this tune forever
+            Come on, Eileen
+            Oh, I swear (what he means)
+            Ah, come on, let's
+            Take off everything
+            That pretty red dress
+            Eileen (tell him yes)
+            Ah, come on, let's
+            Ah, come on, Eileen, please
+            That pretty red dress
+            Eileen (tell him yes)
+            Ah, come on, let's
+            Ah, come on, Eileen, please
+            Now you're full grown (too-ra)
+            And now you have shown (too-ra, ta-loo-ra)
+            Oh, Eileen
+            Said, come on, Eileen (come on, Eileen) (you've grown)
+            These things they are real and I know (ta-loo-rye-ay) (so grown)
+            How you feel (come on, Eileen, ta-loo-rye-ay)
+            Now I must say more than ever (too-ra, ta-loo-ra)
+            Things 'round here have changed
+            I said, too-ra-loo-ra (come on, Eileen, ta-loo-rye-ay)
+            Too-ra-loo-rye-ay (come on, Eileen, ta-loo-rye-ay)
+            (Too-ra, ta-loo-ra)
+            Come on, Eileen
+            Oh, I swear (what he means)
+            At this moment
+            You mean everything
+            You in that dress
+            My thoughts, I confess
+            Verge on dirty
+            Ah, come on, Eileen
+            Come on, Eileen
+            Oh, I swear (what he means)
+            At this moment
+            You mean everything
+            In that dress
+            Oh, my thoughts I confess
+            Well, they're dirty
+            Come on, Eileen
+            Come on, Eileen
+            Whoa (what he means)
+            Whoa
+            Ah, come on, Eileen
+            Oh, believe me
+            If all those endearing young chums
+            That I gaze on so fondly today
+            Were to suddenly leave you, oh fly in the night
+            Just like fairy gifts gone in the sky
+        `,
+        audio: "https://open.spotify.com/embed/track/5bMmTb48ylarEOKNJ334n0?si=3196380d1a654fbc",
+        spotifyEmbedURL: "https://open.spotify.com/embed/track/3MrWxJaD2AT0W9DjWF64Vm?si=9535c92c59274910"
+    },
+    {
+        name: "I Want You",
+        lyrics: `
+            Anytime I need to see your face, I just close my eyes
+            And I am taken to a place where your crystal mind
+            And magenta feelings take up shelter in the base of my spine
+            Sweet like a chic-a-cherry cola
+            I don't need to try to explain; I just hold on tight
+            And if it happens again, I might move so slightly
+            To the arms and the lips and the face of the human cannonball
+            That I need to, I want to
+            
+            Come stand a little bit closer
+            Breathe in and get a bit higher
+            You'll never know what hit you
+            When I get to you
+            
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            
+            I'm the kind of person who endorses a deep commitment
+            Getting comfy, getting perfect is what I live for
+            But a look, then a smell of perfume
+            It's like I'm down on the floor and I don't know what I'm in for
+            Conversation has a time and place
+            In the interaction of a lover and a mate
+            But the time of talking, using symbols, using words
+            Can be likened to a deep sea diver
+            Who is swimming with a raincoat
+            
+            Come stand a little bit closer
+            Breathe in and get a bit higher
+            You'll never know what hit you
+            When I get to you
+            
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            
+            Yeah yeah
+            Yeah yeah
+            
+            Anytime I need to see your face, I just close my eyes
+            And I am taken to a place where your crystal mind
+            And magenta feelings take up shelter in the base of my spine
+            Sweet like a chic-a-cherry cola
+            I don't need to try to explain
+            I just hold on tight
+            And if it happens again, I might move so slightly
+            To the arms and the lips and the face of the human cannonball
+            That I need to, I want to
+            
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out (So can we find out?)
+            
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            Ooh, I want you, I don't know if I need you, but
+            Ooh, I'd die to find out
+            Ooh, I want you, I don't know if I need you but
+            Ooh, I'd die to find out (I'd die to find out)
+            Ooh, I want you, I don't know if I need you, but
+            (Oh, can we find out?)
+            Ooh, I'd die to find out
+        `,
+        audio: "https://open.spotify.com/embed/track/0zMM7lDvm2RxYqQ0eiJ5Tv?si=3196380d1a654fbc",
+        spotifyEmbedURL: "https://open.spotify.com/embed/track/13SKWUEst7c0WohbwPAJJT?si=9535c92c59274910"
+    },
+    {
+        name: "Boombastic",
+        lyrics: `
+            Mr. Boombastic
+            What you want is some Boombastic, romantic, fantastic lover
+            Shaggy
+            Mr. Lover lover, mm
+            Mr. Lover lover, hehe girl
+            Mr. Lover lover, mm
+            Mr. Lover lover
+            She call me Mr. Boombastic, tell me fantastic
+            Touch me inna me back, she say I'm Mr. Ro-mantic
+            Call me fantastic
+            Touch me inna me back she say I'm Mr. Ro...
+            Smooth, just like a silk
+            Soft and cuddly, hug me up like a quilt
+            I'm a lyrical lover, no take me fi no filth
+            With my sexual physique, yah know me well built
+            Oh me, oh my, well, well, can't you tell
+            I'm just like a turtle crawling out of my shell
+            Gal, you captivate my body, put me under a spell
+            With your Khus Khus perfume I love your sweet smell
+            You're the only young girl who can ring my bell
+            And I can take rejection, so you tell me go to hell
+            I'm Boombastic, tell me fantastic
+            Touch me inna me back, she say I'm Mr. Ro-mantic
+            Call me fantastic
+            She touch me inna my back, she says I'm Mr. Boom-boom-boom-boom
+            Boombastic, tell me fantastic
+            Touch me inna my back, she call Mr. Romantic
+            Tell me fantastic
+            She touch me inna my back, she says I'm Mr. Boom-boom
+            Gee whizz, baby please
+            Let me take you to an island of the sweet cool breeze
+            You don't feel like drive, well, baby hand me the keys
+            And I'll take you to a place, and set your mind at ease
+            Don't you tickle my foot bottom, (haha) baby please
+            Don't you play with my nose I'm a (ha-chum) sneeze (bless you)
+            Well you a the bun and me a the cheese
+            And if me ah the rice and baby love you ah the peas
+            I'm Boombastic, tell me fantastic
+            Touch me inna my back, she says I'm Mr. Ro-mantic
+            Tell me fantastic
+            She touch me inna my back, she says I'm Mr. Boom-boom-boom-boom
+            Boombastic, tell me fantastic
+            Touch me inna my back, she call Mr. Ro-mantic
+            Tell me fantastic
+            Touch me inna my back, she says I'm Mr. Boom-boom
+            A me say give me your loving, gal, your loving well, good
+            I want your loving, gal give it like you should
+            Give me your loving, girl, your loving well good
+            I want your loving, gal you remember the woo
+            Would like to kiss and caress
+            Rub down every strand a hair 'pon my chest
+            I'm Boombastic, rated as the best
+            The best you should get, nothing more, nothing less
+            Give me your digits, jot down your address
+            I'll bet you confess, when you put me to the test
+            That I'm Boombastic, tell me fantastic
+            Touch me inna my back, she says I'm Mr. Ro-mantic
+            Tell me fantastic
+            Touch me inna my back, she says I'm Mr. Boombastic
+            Tell me fantastic
+            Touch me inna my back, she call Mr. Ro-mantic
+            Tell me fantastic
+            Touch me inna my back, she says I'm Mr. Boombastic, wha
+            Gal, your admiration, it a lick me from the start
+            With your physical attraction, gal you know to feel the spark
+            A man of few words, nah go tell you no sweet talk
+            Nah go laba laba laba and a chat pure part
+            I'll get straight to the point like a arrow or a dart
+            Come lay down in my jacuzzi, and get some bubble bath
+            Only sound you will here is the beating of my heart
+            And we will mmm mmm, and have some sweet pillow talk
+            I'm Boombastic, tell me fantastic
+            Touch me inna my back, she says I'm Mr. Romantic
+            Tell me fantastic
+            She tickle on my back, she says I'm Mr. Boom
+            Boombastic, tell me fantastic
+            Touch me inna my back, she says I'm Mr. Romantic
+            Tell me fantastic
+            Touch me innna my back, she says I'm Mr. Boom
+            Wha' ya say girl?
+            Smooth
+        `,
+        audio: "https://open.spotify.com/embed/track/7MTTEoEmk94JAXyEY3swcT?si=3196380d1a654fbc",
+        spotifyEmbedURL: "https://open.spotify.com/embed/track/4fxF8ljwryMZX5c9EKrLFE?si=9535c92c59274910"
+    },
+
+   
 ];
 
 export  { availableSongs }
